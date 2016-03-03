34a566625133e60a9194b7f771a97298fd9881f0
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2016-03-03 19:15:53 +0100

    CircleCi - try to run instrumented test in parallel.

---
+++ gradle/connectedCheck.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+echo "starting emulator"
+mksdcard -l e 512M sdcard.img
+emulator -avd circleci-android22 -no-audio -no-window -sdcard sdcard.img
+# Ensure that emulator booted
+circle-android wait-for-boot
+# sleep 30
+echo "unlocking the emulator screen"
+adb shell input keyevent 82
+# Clean logcat
+adb logcat -d
+
+echo "Starting instrumented tests"
+./gradlew spoon -x lint --no-daemon -PdisablePreDex --stacktrace
+
+
+# copy the test results to the test results directory.
+cp -r app/build/spoon/* $CIRCLE_TEST_REPORTS/$CIRCLE_NODE_INDEX # instrumented tests
+adb logcat -d > $CIRCLE_ARTIFACTS/logcat_emulator$CIRCLE_NODE_INDEX.txt # logcat
