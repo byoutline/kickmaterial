5a2d5d1728e2dcf4f9a1854de3801909d66f83d0
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2016-03-04 10:36:15 +0100

    CircleCi - Copy spoon return value to script. Move emulator output to /dev/null.  Sleep before trying to unlock screen.

--- gradle/connectedCheck.sh
+++ gradle/connectedCheck.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "starting emulator"
 mksdcard -l e 512M sdcard.img
-emulator -avd circleci-android22 -no-audio -no-window -sdcard sdcard.img &
+emulator -avd circleci-android22 -no-audio -no-window -sdcard sdcard.img > /dev/null &
 # Ensure that emulator booted
 circle-android wait-for-boot
-# sleep 30
+sleep 30
 echo "unlocking the emulator screen"
 adb shell input keyevent 82
 # Clean logcat
@@ -12,8 +12,9 @@ adb logcat -d
 
 echo "Starting instrumented tests"
 ./gradlew spoon -x lint --no-daemon -PdisablePreDex --stacktrace
-
+rc=$?
 
 # copy the test results to the test results directory.
-cp -r app/build/spoon/* $CIRCLE_TEST_REPORTS/$CIRCLE_NODE_INDEX # instrumented tests
-adb logcat -d > $CIRCLE_ARTIFACTS/logcat_emulator$CIRCLE_NODE_INDEX.txt # logcat
+cp -r app/build/spoon/* $CIRCLE_TEST_REPORTS/ # instrumented tests
+adb logcat -d > $CIRCLE_ARTIFACTS/logcat_emulator.txt # logcat
+exit $rc
