dd143cb3bb2dcf6c185332f722a409e37688fe7b
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2016-03-04 14:49:53 +0100

    CircleCi - Attempt to move configuration back to circle.yml from shell script.

--- gradle/connectedCheck.sh
+++ gradle/connectedCheck.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
-echo "starting emulator"
-mksdcard -l e 512M sdcard.img
-emulator -avd circleci-android22 -no-audio -no-window -sdcard sdcard.img > /dev/null &
-# Ensure that emulator booted
-circle-android wait-for-boot
-sleep 30
-echo "unlocking the emulator screen"
-adb shell input keyevent 82
-# Clean logcat
-adb logcat -d
 
 echo "Starting instrumented tests"
 ./gradlew spoon -x lint --no-daemon -PdisablePreDex --stacktrace
-rc=$?
+rc=$? # Save spoon return value, to return it at the end of the script
 
 # copy the test results to the test results directory.
 cp -r app/build/spoon/* $CIRCLE_TEST_REPORTS/ # instrumented tests
