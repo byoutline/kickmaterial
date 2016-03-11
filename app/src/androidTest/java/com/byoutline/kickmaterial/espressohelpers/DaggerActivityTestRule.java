0c577a34e058cc407651dc7351fba2f6731f0759
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2016-03-11 17:45:51 +0100

    CircleCi: Take screenshot when test fails.

--- app/src/androidTest/java/com/byoutline/kickmaterial/espressohelpers/DaggerActivityTestRule.java
+++ app/src/androidTest/java/com/byoutline/kickmaterial/espressohelpers/DaggerActivityTestRule.java
@@ -20,7 +20,16 @@ import android.app.Activity;
 import android.app.Application;
 import android.support.annotation.NonNull;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.FailureHandler;
+import android.support.test.espresso.base.DefaultFailureHandler;
 import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+import com.squareup.spoon.Spoon;
+import org.hamcrest.Matcher;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+import timber.log.Timber;
 
 /**
  * {@link ActivityTestRule} which provides hook for
@@ -60,5 +69,15 @@ public class DaggerActivityTestRule<T extends Activity> extends ActivityTestRule
     public interface OnBeforeActivityLaunchedListener<T> {
 
         void beforeActivityLaunched(@NonNull Application application, @NonNull T activity);
+    }
+
+    @Override
+    public Statement apply(Statement base, Description description) {
+        // take screenshot if test fails
+        Espresso.setFailureHandler((error, viewMatcher) -> {
+            Spoon.screenshot(getActivity(), error.getClass().getSimpleName(), description.getClassName(), description.getMethodName());
+            new DefaultFailureHandler(getActivity()).handle(error, viewMatcher);
+        });
+        return super.apply(base, description);
     }
 }
