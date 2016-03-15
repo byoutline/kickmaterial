b257d5446461281d45030a167d384a089b4019f8
Sebastian Kacprzak <sebastian.kacprzak@byoutline.com>
2016-03-15 17:20:35 +0100

    CircleCi: disable taking screenshots on non Ci builds, force cached field version.

--- app/src/androidTest/java/com/byoutline/kickmaterial/espressohelpers/DaggerActivityTestRule.java
+++ app/src/androidTest/java/com/byoutline/kickmaterial/espressohelpers/DaggerActivityTestRule.java
@@ -21,15 +21,11 @@ import android.app.Application;
 import android.support.annotation.NonNull;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.Espresso;
-import android.support.test.espresso.FailureHandler;
 import android.support.test.espresso.base.DefaultFailureHandler;
 import android.support.test.rule.ActivityTestRule;
-import android.view.View;
 import com.squareup.spoon.Spoon;
-import org.hamcrest.Matcher;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
-import timber.log.Timber;
 
 /**
  * {@link ActivityTestRule} which provides hook for
@@ -73,11 +69,13 @@ public class DaggerActivityTestRule<T extends Activity> extends ActivityTestRule
 
     @Override
     public Statement apply(Statement base, Description description) {
-        // take screenshot if test fails
-        Espresso.setFailureHandler((error, viewMatcher) -> {
-            Spoon.screenshot(getActivity(), error.getClass().getSimpleName(), description.getClassName(), description.getMethodName());
-            new DefaultFailureHandler(getActivity()).handle(error, viewMatcher);
-        });
+        // On Ci take screenshot if test fails
+        if(System.getenv("CIRCLECI") != null) {
+            Espresso.setFailureHandler((error, viewMatcher) -> {
+                Spoon.screenshot(getActivity(), error.getClass().getSimpleName(), description.getClassName(), description.getMethodName());
+                new DefaultFailureHandler(getActivity()).handle(error, viewMatcher);
+            });
+        }
         return super.apply(base, description);
     }
 }
